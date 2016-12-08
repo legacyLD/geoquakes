6c1d7e44dddc5b64d958b82f469aea6c4632c085
vedelopment <vedelopment@gmail.com>
2016-12-08 05:15:00 -0800

    adds handlebars quake data to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,29 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 
 $(document).on("ready", function() {
 
-  // CODE IN HERE!
-
-});
+  var quakeSource = $('#quake-list').html();//tell variable which element ID contains material for template
+  var quakeTemplate = Handlebars.compile(quakeSource);//run handlebars.compile to mold html material into template to "pour" json data into
+  $.ajax({
+    method: "GET",
+    url: weekly_quakes_endpoint,
+    success: onSuccess,
+    error: onError,
+  });
+  function onSuccess(quakeResults) {
+      var quakeFeatures = quakeResults.features;
+      quakeFeatures.forEach(function(quakeFeatures) {
+      var quakeHtml = quakeTemplate({
+        mag: quakeFeatures.properties.mag,
+        place: quakeFeatures.properties.place,
+      });
+      $('#quake-html').append(quakeHtml);
+      console.log(quakeHtml);
+    });
+    console.log(quakeFeatures);
+  };
+  function onError() {
+    console.log("USGS query did not work!");
+  };
+});//get JSON data to store and later insert into template
